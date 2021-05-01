7dce79a03b32b383e0a7e82e824c129dd93c3174
coding-html <74841670+coding-html@users.noreply.github.com>
2021-05-01 10:20:10 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("myfirstconsoleproject")
+var name = "anay"
+var last_name = "k";
+console.log(last_name, name);
+var names = name.concat(last_name);
+console.log(names);
+var number=20;
+var number2=5;
+var answer= number+number2;
+console.log(answer)
